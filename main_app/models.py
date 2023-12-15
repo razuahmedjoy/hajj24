485229576ee5d23e03bb30b72fa23a91d228bebb
abusaidshabib <abusaidshabib712@gmail.com>
2023-12-16 00:20:17 +0600

    value chanages

--- main_app/models.py
+++ main_app/models.py
@@ -50,5 +50,3 @@ class CameraHeartbeat(models.Model):
     time = models.DateTimeField(null=True, blank=True)
     created_at = models.DateTimeField(default=timezone.now, blank=True, null=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-class 
